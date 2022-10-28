05471687fd69c92a14d6053135bf6daa11cd114f
Manan Khurana <112554344+khuranamanan@users.noreply.github.com>
2022-10-28 18:02:15 +0530

    fix: now user has to selec atleast one button only then it will submit

--- main.js
+++ main.js
@@ -4,13 +4,25 @@ const ratingButtons = document.querySelectorAll(".rating-btn");
 const btnSubmit = document.querySelector("#btn-submit");
 const ratingNumber = document.querySelector(".rating-number");
 
-ratingButtons.forEach((e)=>{
-    e.addEventListener("click", ()=>{
-        ratingNumber.innerText = e.innerText;
-    })
-})
+//Adding Event listner on all rating buttons
+// ratingButtons.forEach((e)=>{
+//     e.addEventListener("click", ()=>{
+//         ratingNumber.innerText = e.innerText;
+//     })
+// })
 
-btnSubmit.addEventListener("click", ()=>{
-    mainContainer.style.display = "none";
-    thankYouContainer.style.display = "";
+// btnSubmit.addEventListener("click", ()=>{
+//     mainContainer.style.display = "none";
+//     thankYouContainer.style.display = "";
+// })
+
+
+ratingButtons.forEach((e) => {
+    e.addEventListener("click", () => {
+        ratingNumber.innerText = e.innerText;
+        btnSubmit.addEventListener("click", () => {
+            mainContainer.style.display = "none";
+            thankYouContainer.style.display = "";
+        })
+    })
 })
